b76a8b426977104e90af1ac7d5e3753020216ad1
elim <elimylkwan@gmail.com>
2021-01-24 03:26:05 +0000

    update route.py

--- server/routes.py
+++ server/routes.py
@@ -2,29 +2,28 @@
 from server import app
 from flask import request, jsonify
 
+# from firebase import Firebase
 import firebase_admin
-from firebase_admin import credentials, db, firestore
+from firebase_admin import credentials, db, firestore, storage
+
+from Sentiment.model import analyse
 
 firebase_admin.initialize_app()
 db = firestore.client()
+storage = storage.bucket("alpaca-72130.appspot.com")
 
 @app.route('/')
 @app.route('/index') #www.alapaca.com/index
 def index():
-    return "hello world"
-
-@app.route('/create-document')
-def create_document():
+    #load the audio file fromt the data base
+    blob = storage.blob("audio_recordings/urdu.wav")
+    blob.download_to_filename("test.wav")
+    sentiment_ans = analyse("./test.wav", "./Sentiment/ada_classifier.model")
+    #upload it to database
     try:
-        db.collection(u'docs').add({'myfirst':'doc'})
+        db.collection(u'sentiment').add({'sentiment':sentiment_ans})
         return "Completed request"
     except Exception as e:
         print(e)
         return 'Could not make request'
     
-
-
-
-
-#www.alpaca.com/ -> hello world
-#www.alpaca.com/index -> hellow world
